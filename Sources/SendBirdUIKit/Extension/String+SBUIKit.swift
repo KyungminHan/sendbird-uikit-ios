c627ef606aa116bd687b47a3168f06fc7c19690f
Kyungmin Han <hans081545@gmail.com>
2022-07-21 19:14:54 +0900

    - 중복된 파일 제거.

--- Sources/SendBirdUIKit/Extension/String+SBUIKit.swift
+++ Sources/SendBirdUIKit/Extension/String+SBUIKit.swift
@@ -15,3 +15,21 @@ extension String {
         }
     }
 }
+
+extension String {
+    func containIndexes(in ranges: [Range<String.Index>]) -> [(start: Int, end: Int)] {
+        return ranges.map {
+            let startIndex = $0.lowerBound.utf16Offset(in: self)
+            let endIndex = $0.upperBound.utf16Offset(in: self)
+            return (start: startIndex, end: endIndex)
+        }
+    }
+
+    func ranges(of targetString: Self, options: String.CompareOptions = [], locale: Locale? = nil) -> [Range<String.Index>] {
+        let result: [Range<String.Index>] = self.indices.compactMap { startIndex in
+            let targetStringEndIndex = index(startIndex, offsetBy: targetString.count, limitedBy: endIndex) ?? endIndex
+            return range(of: targetString, options: options, range: startIndex..<targetStringEndIndex, locale: locale)
+        }
+        return result
+    }
+}
